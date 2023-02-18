bfd2486726f6131bad25ec284e5fcd4531fb61a4
Sim <foday_s@yahoo.com>
2023-02-18 22:21:45 +0000

    sql order by assignment completed

---
+++ problem1.sql
@@ -0,0 +1,11 @@
+SELECT * FROM character ORDER BY last_name, first_name ASC;
+-- SELECT * FROM Customers
+-- ORDER BY Country DESC;
+
+-- SELECT * FROM character WHERE current = true ORDER BY last_name AND first_name DESC
+
+/**
+ * Problem 1: Write a statement below to query the database for 
+all characters. Make sure the results are in
+* ascending order by last name, and first name as a tie-breaker.
+         */
